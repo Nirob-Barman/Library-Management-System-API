3c8d5267f5debc246c8428e1697ba7d25677d349
Nirob-Barman <niroborin111@gmail.com>
2024-11-12 08:10:46 +0600

    Book CRUD Operation

--- src/app/modules/book/book.service.ts
+++ src/app/modules/book/book.service.ts
@@ -1,7 +1,33 @@
 import prisma from "../../shared/prisma";
 
 const createBook = async (payload: any) => {
-    const newBook = await prisma.book.create({
+    const cretedBook = await prisma.book.create({
+        data: {
+            title: payload.title,
+            genre: payload.genre,
+            publishedYear: payload.publishedYear,
+            totalCopies: payload.totalCopies,
+            availableCopies: payload.availableCopies,
+        },
+    });
+    return cretedBook;
+};
+
+const getAllBooksFromDB = async () => {
+    const books = await prisma.book.findMany();
+    return books;
+};
+
+const getBookByIdFromDB = async (bookId: string) => {
+    const book = await prisma.book.findUnique({
+        where: { bookId },
+    });
+    return book;
+};
+
+const updateBookIntoDB = async (bookId: string, payload: any) => {
+    const updatedBook = await prisma.book.update({
+        where: { bookId },
         data: {
             title: payload.title,
             genre: payload.genre,
@@ -11,9 +37,21 @@ const createBook = async (payload: any) => {
         },
     });
 
-    return newBook;
+    return updatedBook;
+};
+
+const deleteBookFromDB = async (bookId: string) => {
+    const deletedBook = await prisma.book.delete({
+        where: { bookId },
+    });
+
+    return deletedBook;
 };
 
 export const BookService = {
-    createBook
+    createBook,
+    getAllBooksFromDB,
+    getBookByIdFromDB,
+    updateBookIntoDB,
+    deleteBookFromDB
 };
