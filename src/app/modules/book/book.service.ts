acc2c2f70f78a01341ae31379b8138a442dcfdd8
Nirob-Barman <niroborin111@gmail.com>
2024-11-12 21:26:22 +0600

    Book Serice Error Handling Updated

--- src/app/modules/book/book.service.ts
+++ src/app/modules/book/book.service.ts
@@ -1,6 +1,34 @@
 import prisma from "../../shared/prisma";
+import { validate as isUUID } from 'uuid';
+import { BadRequestError, ConflictError, NotFoundError } from "../../utils/error";
+
+const getBookById = async (bookId: string) => {
+    if (!isUUID(bookId)) {
+        throw new BadRequestError(`Invalid book ID format. Please provide a valid UUID.`);
+    }
+
+    const book = await prisma.book.findUnique({
+        where: { bookId },
+    });
+
+    if (!book) {
+        throw new NotFoundError(`Sorry, the book with ID ${bookId} could not be found. Please check the ID and try again.`);
+    }
+
+    return book;
+};
 
 const createBook = async (payload: any) => {
+    const existingBook = await prisma.book.findFirst({
+        where: {
+            title: payload.title,
+        },
+    });
+
+    if (existingBook) {
+        throw new ConflictError(`A book with the title ${payload.title} already exists.`);
+    }
+    
     const cretedBook = await prisma.book.create({
         data: {
             title: payload.title,
@@ -19,13 +47,15 @@ const getAllBooksFromDB = async () => {
 };
 
 const getBookByIdFromDB = async (bookId: string) => {
-    const book = await prisma.book.findUnique({
-        where: { bookId },
-    });
-    return book;
+    // const book = await prisma.book.findUnique({
+    //     where: { bookId },
+    // });
+    // return book;
+    return await getBookById(bookId);
 };
 
 const updateBookIntoDB = async (bookId: string, payload: any) => {
+    await getBookById(bookId);
     const updatedBook = await prisma.book.update({
         where: { bookId },
         data: {
@@ -41,6 +71,7 @@ const updateBookIntoDB = async (bookId: string, payload: any) => {
 };
 
 const deleteBookFromDB = async (bookId: string) => {
+    await getBookById(bookId);
     const deletedBook = await prisma.book.delete({
         where: { bookId },
     });
